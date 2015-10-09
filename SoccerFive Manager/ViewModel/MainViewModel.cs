0b99788dde55457d92f4c29931510b95c3da6b6e
qmidy <midy.quentin@live.fr>
2015-10-10 00:36:37 +0200

    Initialisation de la solution
    Module Commun et Main Menu initialisés

---
+++ SoccerFive Manager/ViewModel/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommonLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Main
+{
+    public class MainViewModel : CommonViewModel
+    {
+        private UserControl mainContent;
+        public UserControl MainContent
+        {
+            get
+            {
+                return mainContent;
+            }
+            set
+            {
+                mainContent = value;
+                OnPropertyChanged("MainContent");
+            }
+        }
+        public MainViewModel()
+        {
+            // Factory à implémenter pour la création des views et des viewmodels
+            MainContent = new MainMenuModule.MainMenuView();
+        }
+    }
+}
