bd79550ab86937274d4d0d75b1f495c62a897d7e
qmidy <midy.quentin@live.fr>
2016-03-22 22:20:43 +0100

    Mise à jour bdd

--- SoccerFive Manager/bin/Debug/SqlScripts/CreateGameScript.sql
+++ SoccerFive Manager/bin/Debug/SqlScripts/CreateGameScript.sql
@@ -2,13 +2,19 @@
 
 CREATE TABLE club (id INT NOT NULL PRIMARY KEY, name VARCHAR(20));
 
-CREATE TABLE team (id INT NOT NULL PRIMARY KEY, club_id INT,  FOREIGN KEY (club_id) REFERENCES club(id));
+CREATE TABLE championship(id INT NOT NULL PRIMARY KEY);
+
+CREATE TABLE team (id INT NOT NULL PRIMARY KEY, club_id INT, championship_id INT, FOREIGN KEY (club_id) REFERENCES club(id), FOREIGN KEY (championship_id) REFERENCES championship(id));
 
 CREATE TABLE player (id INT NOT NULL PRIMARY KEY, name VARCHAR(20), team_id INT, area INT, number INT, attack INT, defense INT, FOREIGN KEY (team_id) REFERENCES team(id));
 
+CREATE TABLE match (id INT NOT NULL PRIMARY KEY, team1_id INT, team2_id INT, championship_id INT, score1 INT, score2 INT, FOREIGN KEY (team1_id) REFERENCES team(id), FOREIGN KEY (team2_id) REFERENCES team(id), FOREIGN KEY (championship_id) REFERENCES championship(id));
+
 INSERT INTO club VALUES ('1','{0}');
 
-INSERT INTO team VALUES ('1','1');
+INSERT INTO championship VALUES ('1');
+
+INSERT INTO team VALUES ('1','1','1');
 
 INSERT INTO player VALUES ('1','Tom','1','0','1','10','10');
 INSERT INTO player VALUES ('2','Dédé','1','0','2','10','10');
@@ -17,13 +23,13 @@ INSERT INTO player VALUES ('4','Eric','1','0','4','10','10');
 INSERT INTO player VALUES ('5','Pierre','1','0','5','10','10');
 
 INSERT INTO club VALUES ('2','Club2');
-INSERT INTO team VALUES ('2','2');
+INSERT INTO team VALUES ('2','2','1');
 
 INSERT INTO club VALUES ('3','Club3');
-INSERT INTO team VALUES ('3','3');
+INSERT INTO team VALUES ('3','3','1');
 
 INSERT INTO club VALUES ('4','Club4');
-INSERT INTO team VALUES ('4','4');
+INSERT INTO team VALUES ('4','4','1');
 
 INSERT INTO player VALUES ('6','Player1','2','0','1','10','10');
 INSERT INTO player VALUES ('7','Player2','2','0','2','10','10');
